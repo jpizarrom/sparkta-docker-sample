e0148fe8579d9847ccaebb2af0de106066488775
jpizarrom <jpizarrom@gmail.com>
2016-10-04 22:28:17 -0300

    sparta 0.11.0 (#1)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,41 +1,90 @@
 #!/bin/bash -xe
- # sed -i "s|executionMode.*|executionMode = \"${EXECUTION_MODE:=local}\"|" /etc/sds/sparta/application.conf
- # sed -i "s|connectionString.*|connectionString = \""${ZOOKEEPER_HOST:=localhost:2181}"\"|" /etc/sds/sparta/application.conf
- # sed -i "s|hdfsMaster.*|hdfsMaster = \"${HDFS_MASTER:=localhost}\"|" /etc/sds/sparta/application.conf
+ # SPARTA_CONF_FILE=/etc/sds/sparta/application.conf
+  if [[ ! -v SPARK_VERSION ]]; then
+   SPARK_VERSION=spark-1.6.2
+ fi
+ if [[ ! -v SPARK_BINARY_URL ]]; then
+   SPARK_BINARY_URL=http://apache.rediris.es/spark
+ fi
+ if [[ ! -v HADOOP_SPARK_VERSION ]]; then
+   HADOOP_SPARK_VERSION=hadoop2.6
+ fi
+ if [[ ! -v HADOOP_VERSION ]]; then
+   HADOOP_VERSION=hadoop-2.7.1
+ fi
+ if [[ ! -v HADOOP_BINARY_URL ]]; then
+   HADOOP_BINARY_URL=http://www.eu.apache.org/dist/hadoop/common
+ fi
+ if [[ ! -v EXECUTION_MODE ]]; then
+   EXECUTION_MODE=local
+ fi
+ if [[ ! -v ZOOKEEPER_HOST ]]; then
+   ZOOKEEPER_HOST=localhost:2181
+ fi
+ if [[ ! -v HDFS_MASTER ]]; then
+   HDFS_MASTER=localhost
+ fi
+ if [[ ! -v HDFS_PORT ]]; then
+   HDFS_PORT=8020
+ fi
+ if [[ ! -v HDFS_USER_NAME ]]; then
+   HDFS_USER_NAME=stratio
+ fi
+ if [[ ! -v SPARK_MASTER ]]; then
+   SPARK_MASTER="local[*]"
+ fi
+   if [[ ! -v MESOS_MASTER ]]; then
+   MESOS_MASTER=localhost:7077
+ fi
+ # sed -i "s|executionMode.*|executionMode = \"${EXECUTION_MODE}\"|" ${SPARTA_CONF_FILE}
+ # sed -i "s|connectionString.*|connectionString = \""${ZOOKEEPER_HOST}"\"|" ${SPARTA_CONF_FILE}
+ # sed -i "s|hdfsMaster.*|hdfsMaster = \"${HDFS_MASTER}\"|" ${SPARTA_CONF_FILE}
+ # sed -i "s|hdfsPort.*|hdfsPort = \"${HDFS_PORT}\"|" ${SPARTA_CONF_FILE}
+ # sed -i "s|hadoopUserName.*|hadoopUserName = \"${HDFS_USER_NAME}\"|" ${SPARTA_CONF_FILE}
  # if [[ ! -v EXECUTION_MODE || "${EXECUTION_MODE}" == "local" ]]; then
- #   sed -i "s|spark.master.*|spark.master = \""${SPARK_MASTER:=local[4]}"\"|" /etc/sds/sparta/application.conf
+ #   sed -i "s|spark.master.*|spark.master = \"${SPARK_MASTER}\"|" ${SPARTA_CONF_FILE}
  # fi
  if [[ "${EXECUTION_MODE}" == "mesos" || "${EXECUTION_MODE}" == "yarn" || "${EXECUTION_MODE}" == "standalone" ]]; then
-   # wget http://tools.stratio.com/spark/spark-1.5.2/spark-1.5.2-bin-hadoop2.6.tgz
-   # wget http://www.apache.org/dyn/closer.lua/spark/spark-1.5.2/spark-1.5.2-bin-hadoop2.6.tgz
-
-   # wget http://192.168.0.18:8000/spark-1.5.2-bin-hadoop2.6.tgz
-   mkdir -p /opt/sds/spark
-   # tar xvf spark-1.5.2-bin-hadoop2.6.tgz -C /opt/sds/spark --strip-components=1
-   # rm spark-1.5.2-bin-hadoop2.6.tgz
-
-   # wget http://192.168.0.18:8000/spark-1.6.1-bin-hadoop2.6.tgz
-   # wget https://www.apache.org/dist/spark/spark-1.6.1/spark-1.6.1-bin-hadoop2.6.tgz
-   wget $SPARK_BINARY_URL
-   mkdir -p /opt/sds/spark
-   tar xvf spark-1.6.1-bin-hadoop2.6.tgz -C /opt/sds/spark --strip-components=1
-   rm spark-1.6.1-bin-hadoop2.6.tgz
-
-   echo "" >> /etc/default/sparta-variables
-   echo "export HADOOP_HOME="/opt/sds/hadoop"" >> /etc/default/sparta-variables
-   echo "export HADOOP_USER_NAME="stratio"" >> /etc/default/sparta-variables
-   echo "export HADOOP_CONF_DIR="/opt/sds/hadoop/conf"" >> /etc/default/sparta-variables
+   SPARK_HADOOP_VERSION_FILE="${SPARK_VERSION}-bin-${HADOOP_SPARK_VERSION}.tgz"
+   HADOOP_VERSION_FILE="${HADOOP_VERSION}.tar.gz"
+   CORE_SITE=core-site.xml
+   HDFS_SITE=hdfs-site.xml
+   YARN_SITE=yarn-site.xml.xml
+   MAPRED_SITE=mapred-site.xml
+   SPARK_HOME="/${SPARK_VERSION}-bin-${HADOOP_SPARK_VERSION}"
+   SPARTA_VARIABLES=/etc/default/sparta-variables
+   wget "${SPARK_BINARY_URL}/${SPARK_VERSION}/${SPARK_HADOOP_VERSION_FILE}"
+   tar -xvzf ${SPARK_HADOOP_VERSION_FILE}
+   rm ${SPARK_HADOOP_VERSION_FILE}
+   wget "${HADOOP_BINARY_URL}/${HADOOP_VERSION}/${HADOOP_VERSION_FILE}"
+   tar -xvzf ${HADOOP_VERSION_FILE}
+   rm ${HADOOP_VERSION_FILE}
+   mkdir "/${HADOOP_VERSION}/conf"
+   wget "http://${HDFS_MASTER}:50070/conf"
+   cp conf "/${HADOOP_VERSION}/conf/${CORE_SITE}"
+   cp conf "/${HADOOP_VERSION}/conf/${HDFS_SITE}"
+   cp conf "/${HADOOP_VERSION}/conf/${YARN_SITE}"
+   cp conf "/${HADOOP_VERSION}/conf/${MAPRED_SITE}"
+   rm conf
+   # echo "" >> ${SPARTA_VARIABLES}
+   # echo "export HADOOP_HOME="/${HADOOP_VERSION}"" >> ${SPARTA_VARIABLES}
+   # echo "export HADOOP_USER_NAME="/${HDFS_USER_NAME}"" >> ${SPARTA_VARIABLES}
+   # echo "export HADOOP_CONF_DIR="/${HADOOP_VERSION}/conf"" >> ${SPARTA_VARIABLES}
+   # echo "export SPARK_HOME="${SPARK_HOME}"" >> ${SPARTA_VARIABLES}
+   # source "${SPARTA_VARIABLES}"
+   # sed -i "/# checkpointPath/d" ${SPARTA_CONF_FILE}
+   # sed -i "s|checkpointPath.*|checkpointPath = \"/user/stratio/sparta/checkpoint\"|" ${SPARTA_CONF_FILE}
  fi
  # if [[ "${EXECUTION_MODE}" == "mesos" ]]; then
- #   sed -i "s|sparkHome.*|sparkHome = \""${SPARK_HOME}"\"|" /etc/sds/sparta/application.conf
- #   sed -i "s|master = \"mesos.*|master = \""mesos://${MESOS_MASTER}:7077"\"|" /etc/sds/sparta/application.conf
+ #   sed -i "s|sparkHome.*|sparkHome = \""${SPARK_HOME}"\"|" ${SPARTA_CONF_FILE}
+ #   sed -i "s|master = \"mesos.*|master = \""mesos://${MESOS_MASTER}"\"|" ${SPARTA_CONF_FILE}
  # fi
  # if [[ "${EXECUTION_MODE}" == "yarn" ]]; then
- #   sed -i "s|sparkHome.*|sparkHome = \""${SPARK_HOME}"\"|" /etc/sds/sparta/application.conf
+ #   sed -i "s|sparkHome.*|sparkHome = \""${SPARK_HOME}"\"|" ${SPARTA_CONF_FILE}
  # fi
  # if [[ "${EXECUTION_MODE}" == "standalone" ]]; then
- #   sed -i "s|sparkHome.*|sparkHome = \""${SPARK_HOME}"\"|" /etc/sds/sparta/application.conf
- #   sed -i "s|master = \"spark.*|master = \"spark://"${SPARK_MASTER:=localhost}":7077\"|" /etc/sds/sparta/application.conf
+ #   sed -i "s|sparkHome.*|sparkHome = \""${SPARK_HOME}"\"|" ${SPARTA_CONF_FILE}
+ #   sed -i "s|master = \"spark.*|master = \""spark://${SPARK_MASTER}"\"|" ${SPARTA_CONF_FILE}
  # fi
  if [[ "${SSH}" == "true" ]]; then
    /usr/sbin/sshd -e
